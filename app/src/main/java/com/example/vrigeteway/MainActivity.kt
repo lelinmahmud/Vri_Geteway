195d080d57343a8162cdbdafa1681fa6af0f596a
Lelin Mahmud <lelinaits20@gmail.com>
2021-07-12 15:53:17 +0600

    scrollview added in layout

--- app/src/main/java/com/example/vrigeteway/MainActivity.kt
+++ app/src/main/java/com/example/vrigeteway/MainActivity.kt
@@ -9,14 +9,6 @@ import android.view.WindowManager
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        requestWindowFeature(Window.FEATURE_NO_TITLE);
-//
-//
-//        window.setFlags(
-//            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS ,
-//            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-//        )
-//        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
 
         setContentView(R.layout.activity_main)
     }
