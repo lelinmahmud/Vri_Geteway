c395ba0814429e18781b6d22370c46a9a0d9c581
Lelin Mahmud <lelinaits20@gmail.com>
2021-07-12 14:46:00 +0600

    login page done

--- app/src/main/java/com/example/vrigeteway/MainActivity.kt
+++ app/src/main/java/com/example/vrigeteway/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.vrigeteway
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//
+//
+//        window.setFlags(
+//            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS ,
+//            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+//        )
+//        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
         setContentView(R.layout.activity_main)
     }
 }
